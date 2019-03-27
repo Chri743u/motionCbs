fd19cf467114d7a2e5ad90d8cac610e83a24d92b
nadiadejgaard <nadia.dejgaard@hotmail.dk>
2019-03-27 08:51:53 +0100

    hej

--- src/com/motionCBS/client/UI/login/loginView.java
+++ src/com/motionCBS/client/UI/login/loginView.java
@@ -2,16 +2,49 @@ package com.motionCBS.client.UI.login;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.event.dom.client.ClickHandler;
+
 
 public class loginView extends Composite {
+
+    private static loginViewUiBinder UiBinder = GWT.create(loginViewUiBinder.class);
+    @UiField
+    TextBox usernameTxtBox;
+    @UiField
+    PasswordTextBox passwordTxtBox;
+    @UiField
+    Button loginBtn;
+    @UiField
+    Button registerBtn;
+
     interface loginViewUiBinder extends UiBinder<HTMLPanel, loginView> {
     }
 
-    private static loginViewUiBinder ourUiBinder = GWT.create(loginViewUiBinder.class);
-
     public loginView() {
-        initWidget(ourUiBinder.createAndBindUi(this));
+
+        initWidget(UiBinder.createAndBindUi(this));
+
+        usernameTxtBox.getElement().setPropertyString("placeholder","username");
+        passwordTxtBox.getElement().setPropertyString("placeholder", "password");
     }
+
+    // Method to add a Click Handler to the login button
+    public void addClickHandlers(ClickHandler clickHandler) {
+        loginBtn.addClickHandler(clickHandler);
+        registerBtn.addClickHandler(clickHandler);
+    }
+
+    // Method to clean the tectbox fiels
+    public void clearTextBoxFields(){
+        usernameTxtBox.setText("");
+        passwordTxtBox.setText("");
+    }
+
+    // Getters for the usernameTxtBox and passwordTxtBox
+    public TextBox getUsernameTxtBox() { return usernameTxtBox; }
+    public PasswordTextBox getPasswordTxtBox() { return passwordTxtBox; }
 }
