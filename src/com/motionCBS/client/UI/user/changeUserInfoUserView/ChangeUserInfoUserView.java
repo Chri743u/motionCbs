a78b82caab318be3e22550168d768558b8d70706
nadiadejgaard <nadia.dejgaard@hotmail.dk>
2019-03-27 12:16:08 +0100

    h

--- src/com/motionCBS/client/UI/user/changeUserInfoUserView/ChangeUserInfoUserView.java
+++ src/com/motionCBS/client/UI/user/changeUserInfoUserView/ChangeUserInfoUserView.java
@@ -5,6 +5,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.*;
+import com.motionCBS.Shared.User;
 
 public class ChangeUserInfoUserView extends Composite {
 
@@ -27,7 +28,7 @@ public class ChangeUserInfoUserView extends Composite {
     @UiField
     ListBox lstHoursPrWeek;
     @UiField
-    PasswordTextBox txtPassword;
+    TextBox txtPassword;
     @UiField
     ListBox lstTeamtype;
     @UiField
@@ -37,13 +38,45 @@ public class ChangeUserInfoUserView extends Composite {
 
     public ChangeUserInfoUserView() {initWidget(ourUiBinder.createAndBindUi(this));}
 
+
     // This method is adding the click handler to the change profile button
     public void addClickHandlers(ClickHandler clickHandler) {
         changeProfileBtn.addClickHandler(clickHandler);
     }
 
+
     // This method is used to set all the widgets with the current users information
     public void setProfileChanges(User user) {
-        txtFname.setText(user.getFirstName);
+        txtFname.setText(user.getFname());
+        txtLname.setText(user.getLname());
+        txtEmail.setText(user.getEmail());
+        txtAddress.setText(user.getAddress());
+        txtMobileNo.setTabIndex(user.getMobilenr()); // Måske forkert??
+        txtEducation.setText(user.getEducation());
+        lstExperience.setTabIndex(user.getExperience()); // Måske forkert??
+        lstHoursPrWeek.setTabIndex(user.getHoursPrWeek()); // Måske forkert??
+        txtPassword.setText(user.getPassword());
+        lstTeamtype.setName(user.getTeamtype()); // måske forkert??
     }
+
+    // Getters
+    public TextBox getTxtFname() { return txtFname; }
+
+    public TextBox getTxtLname() { return txtLname; }
+
+    public TextBox getTxtEmail() { return txtEmail; }
+
+    public TextBox getTxtAddress() { return txtAddress; }
+
+    public TextBox getTxtMobileNo() { return txtMobileNo; }
+
+    public TextBox getTxtEducation() { return txtEducation; }
+
+    public ListBox getLstExperience() { return lstExperience; }
+
+    public ListBox getLstHoursPrWeek() { return lstHoursPrWeek; }
+
+    public TextBox getTxtPassword() { return txtPassword; }
+
+    public ListBox getLstTeamtype() { return lstTeamtype; }
 }
