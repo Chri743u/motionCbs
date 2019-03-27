73050ba1098596b2f9685565b82434975b7c6d7a
Lasse <lasse_sl@me.com>
2019-03-27 10:20:54 +0100

    Hej

--- src/com/motionCBS/client/UI/user/changeUserInfoUserView/ChangeUserInfoUserView.java
+++ src/com/motionCBS/client/UI/user/changeUserInfoUserView/ChangeUserInfoUserView.java
@@ -1,17 +1,16 @@
 package com.motionCBS.client.UI.user.changeUserInfoUserView;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTMLPanel;
 
-public class ChangeUserInfoUserView extends Composite {
-    interface changeUserInfoUserViewUiBinder extends UiBinder<HTMLPanel, ChangeUserInfoUserView> {
+public class ChangeUserInfoUserView {
+    interface ChangeUserInfoUserViewUiBinder extends UiBinder<DivElement, ChangeUserInfoUserView> {
     }
 
-    private static changeUserInfoUserViewUiBinder ourUiBinder = GWT.create(changeUserInfoUserViewUiBinder.class);
+    private static ChangeUserInfoUserViewUiBinder ourUiBinder = GWT.create(ChangeUserInfoUserViewUiBinder.class);
 
     public ChangeUserInfoUserView() {
-        initWidget(ourUiBinder.createAndBindUi(this));
+        DivElement rootElement = ourUiBinder.createAndBindUi(this);
     }
 }
