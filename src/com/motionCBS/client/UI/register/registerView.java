48f035137695a7d14bbc732feeeacf07c8b3e6b7
nikolajno <nikolaj_norborg@hotmail.com>
2019-03-27 09:24:02 +0100

    hej

--- src/com/motionCBS/client/UI/register/registerView.java
+++ src/com/motionCBS/client/UI/register/registerView.java
@@ -2,11 +2,25 @@ package com.motionCBS.client.UI.register;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.*;
 
 public class registerView extends Composite {
+    private VerticalPanel vPanel;
+
+    private TextBox txtFname;
+    private TextBox txtLname;
+    private TextBox txtEmail;
+    private TextBox txtAddress;
+    private TextBox txtMobileNo;
+    private TextBox txtEducation;
+    private ListBox lstExperience;
+    private ListBox lstHoursPrWeek;
+    private PasswordTextBox txtPassword;
+    private ListBox lstTeamtype;
+
     interface registerViewUiBinder extends UiBinder<HTMLPanel, registerView> {
+
+
     }
 
     private static registerViewUiBinder ourUiBinder = GWT.create(registerViewUiBinder.class);
